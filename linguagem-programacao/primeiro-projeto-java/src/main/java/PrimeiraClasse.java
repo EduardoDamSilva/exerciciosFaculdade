011832695e9ec5aa6f13ed9486e658ad522bf61a
EduardoDamSilva <eduardo.silva@sptech.school>
2023-02-10 12:16:42 -0300

    criando novas pastas e organizando as antigas para esse semestre

---
+++ linguagem-programacao/primeiro-projeto-java/src/main/java/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+
+
+/**
+ *
+ * @author edubi
+ */
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        Integer numero = 42;
+        
+        System.out.println("Meu número é: " + numero);
+    }
+}
