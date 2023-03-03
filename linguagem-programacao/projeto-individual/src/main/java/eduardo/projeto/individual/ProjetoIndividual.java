e2fa6f8909b36c71a314d7627ae9baf3cca7bbdf
EduardoDamSilva <eduardo.silva@sptech.school>
2023-03-03 18:11:46 -0300

    funcionalidade adicionada no case 4 com um jogo

--- linguagem-programacao/projeto-individual/src/main/java/eduardo/projeto/individual/ProjetoIndividual.java
+++ linguagem-programacao/projeto-individual/src/main/java/eduardo/projeto/individual/ProjetoIndividual.java
@@ -22,14 +22,15 @@ public class ProjetoIndividual {
                             + "1 - Calculo de Armadura\n" 
                             + "2 - Descubra seu estilo de jogo\n" 
                             + "3 - Descubra a % dos jogares que estão abaixo de você\n" 
-                            + "4 - Subtração\n"
-                            + "5 - Potência\n"
+                            + "4 - Quem disse isso? (Jogo)\n"
                             + "0 - Sair"
+                            
             );
             numeroOperacao = sc.nextInt();
             System.out.println("");
             switch(numeroOperacao){
                 case 0:
+                    System.out.println("Boa gameplay, até!");
                     break;
                 case 1:
                     System.out.println("O quanto de armadura o campeão tem?");
@@ -54,15 +55,19 @@ public class ProjetoIndividual {
                     switch(numeroClasse){
                         case 1:
                             System.out.println("Top lane e Jungle");
-                        break;
+                            break;
                         case 2:
                             System.out.println("Mid lane, Jungle e Suporte");
+                            break;
                         case 3:
                             System.out.println("Mid lane e Jungle");
+                            break;
                         case 4: 
                             System.out.println("Adc(Bot lane)");
+                            break;
                         case 5:
                             System.out.println("Jungle e Suporte(Bot lane)");
+                            break;
                         default:
                             System.out.println("Insira um número válido");
                     }} while(numeroClasse < 1 || numeroClasse > 5);
@@ -71,14 +76,14 @@ public class ProjetoIndividual {
                     Integer numeroElo;
                     do{System.out.println(
                             "Digite o número que represente o seu elo:\n"
-                                    + "1 - Desafiante"
-                                    + "2 - Grão-Mestre"
-                                    + "3 - Mestre"
-                                    + "4 - Diamante"
-                                    + "5 - Platina"
-                                    + "6 - Ouro"
-                                    + "7 - Prata"
-                                    + "8 - Bronze"
+                                    + "1 - Desafiante\n"
+                                    + "2 - Grão-Mestre\n"
+                                    + "3 - Mestre\n"
+                                    + "4 - Diamante\n"
+                                    + "5 - Platina\n"
+                                    + "6 - Ouro\n"
+                                    + "7 - Prata\n"
+                                    + "8 - Bronze\n"
                                     + "9 - Ferro"
                     );
                     numeroElo = sc.nextInt();
@@ -111,13 +116,101 @@ public class ProjetoIndividual {
                         case 9:
                             System.out.println("Você estando no Ferro não está acima de ninguém :')");
                             break;
+                        default:
+                            System.out.println("Digite um número válido.");
                     }
                     }while(numeroElo < 1 || numeroElo > 9);
                     break;
-                //case 4:
-                   // break;
-                //case 5:
-                   // break;
+                case 4:
+                    Integer jogarNovamenteNumero;
+                    do{
+                    Integer pontuacao = 0;
+                    Integer numeroAlternativa;
+                    do{
+                        System.out.println("Nunca torne-se um monstro para derrotar outro.\n"
+                            + "1 - Kayle\n"
+                            + "2 - Morgana\n"
+                            + "3 - Karma\n"
+                            + "4 - Master Yi");
+                        numeroAlternativa = sc.nextInt();
+                        switch(numeroAlternativa){
+                            case 1:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            case 2:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            case 3:
+                                System.out.println("Reposta Correta! + 50 pontos");
+                                pontuacao += 50;
+                                break;
+                            case 4:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            default:
+                                System.out.println("Digite um número válido.");
+                        }
+                        
+                            
+                    }while(numeroAlternativa < 1 || numeroAlternativa > 4);
+                    
+                    do{
+                        System.out.println("A escalada é longa, mas a vista vale a pena!\n"
+                            + "1 - Taric\n"
+                            + "2 - Braum\n"
+                            + "3 - Jhin\n"
+                            + "4 - Ornn");
+                        numeroAlternativa = sc.nextInt();
+                        switch(numeroAlternativa){
+                            case 1:
+                                System.out.println("Reposta Correta! + 50 pontos");
+                                pontuacao += 50;
+                                break;
+                            case 2:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            case 3:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            case 4:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            default:
+                                System.out.println("Digite um número válido.");
+                        }
+                    } while(numeroAlternativa < 1 || numeroAlternativa > 4);
+                    
+                    do{
+                        System.out.println("A verdadeira sabedoria é reconhecer o valor da própria ignorância\n"
+                            + "1 - Thresh\n"
+                            + "2 - Renekton\n"
+                            + "3 - Aphelions\n"
+                            + "4 - Lee sin");
+                        numeroAlternativa = sc.nextInt();
+                        switch(numeroAlternativa){
+                            case 1:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            case 2:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            case 3:
+                                System.out.println("Resposta Errada!");
+                                break;
+                            case 4:
+                                System.out.println("Reposta Correta! + 50 pontos");
+                                pontuacao += 50;
+                                break;
+                            default:
+                                System.out.println("Digite um número válido.");
+                        }
+                    } while(numeroAlternativa < 1 || numeroAlternativa > 4);
+                    System.out.println("Pontuação total: " + pontuacao);
+                    System.out.println("Deseja jogar novamente? \n"
+                            + "1 - Sim\n"
+                            + "2 - Não");
+                    jogarNovamenteNumero = sc.nextInt();
+                    }while(jogarNovamenteNumero == 1);
             }
         }while(numeroOperacao != 0);
     }
